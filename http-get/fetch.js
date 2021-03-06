48d7cce00551a430393c78a3d41131313261dd39
Tekla <davidannatekla@gmail.com>
2021-03-06 10:52:29 +0100

    add first exercise

--- http-get/fetch.js
+++ http-get/fetch.js
@@ -1,4 +1,16 @@
-//can be deleted
-console.log("Console log works!");
+fetch("https://jsonplaceholder.typicode.com/usersr")
+    .then(respones => {
+        if (!respones.ok) {
+            throw Error(respones.status);
+        } else {
+            return respones.json();
+        }
+    }).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error(error);
+})
+
+
 
 
